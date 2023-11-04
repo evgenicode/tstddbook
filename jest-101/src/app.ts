802bb9f82522f23a0f795368a5296902134cedc4
evmatv <evgenii.contact@gmail.com>
2023-11-04 05:30:48 +0200

    initial commit

---
+++ jest-101/src/app.ts
@@ -0,0 +1,18 @@
+const letters = "ABCDEFGHIJKLMNOPORSTUVWXYZ";
+
+export const convert = (str: string) => {
+  return str
+    .split("")
+    .map((c) => {
+      const index = letters.indexOf(c);
+      if (index !== -1) {
+        if (index + 13 >= 26) {
+          return letters[index + 13 - 26];
+        } else {
+          return letters[index + 13];
+        }
+      }
+      return c;
+    })
+    .join("");
+};
---
+++ jest-101/src/calc.test.ts
@@ -0,0 +1,16 @@
+import { convert } from "./app";
+
+describe("ROT13", () => {
+  it("returns N when given A", () => {
+    expect(convert("A")).toEqual("N");
+  });
+  it("return A when given N", () => {
+    expect(convert("N")).toEqual("A");
+  });
+  it("return NO when given AB", () => {
+    expect(convert("AB")).toEqual("NO");
+  });
+  it("return AB!! when given NO!!", () => {
+    expect(convert("NO!!")).toEqual("AB!!");
+  });
+});
