61765bc3e8a8c42288fb25314079465e725b1f4d
evmatv <evgenii.contact@gmail.com>
2023-11-14 07:36:23 +0200

    fix cypress async issues

--- bookish-react/cypress/e2e/bookish.spec.cy.ts
+++ bookish-react/cypress/e2e/bookish.spec.cy.ts
@@ -1,6 +1,29 @@
 /// <reference types="cypress" />
 
 describe("Bookish application", function () {
+  before(() => {
+    cy.request("DELETE", "http://localhost:8080/books?_cleanup=true");
+  });
+
+  afterEach(() => {
+    cy.request("DELETE", "http://localhost:8080/books?_cleanup=true");
+  });
+  beforeEach(() => {
+    const books = [
+      { name: "Refactoring", id: 1 },
+      { name: "Domain-driven design", id: 2 },
+      { name: "Building Microservices", id: 3 },
+    ];
+    cy.wrap(books).each((item) => {
+      cy.request({
+        method: "POST",
+        url: "http://localhost:8080/books",
+        headers: { "Content-Type": "application/json" },
+        body: item,
+      });
+    });
+  });
+
   it("Visists the bookish", function () {
     cy.visit("http://localhost:3000/");
     cy.get("h2[data-test='heading']").contains("Bookish");
@@ -9,7 +32,7 @@ describe("Bookish application", function () {
   it("Shows a book list", () => {
     cy.visit("http://localhost:3000/");
     cy.get("div[data-test='book-list']").should("exist");
-    cy.get("div.book-item").should("have.length", 2);
+    cy.get("div.book-item").should("have.length", 3);
   });
 
   it("Shows a book list2", () => {
@@ -17,10 +40,14 @@ describe("Bookish application", function () {
 
     cy.get("div[data-test='book-list']").should("exist");
     cy.get("div.book-item").should((books) => {
-      expect(books).to.have.length(2);
+      expect(books).to.have.length(3);
 
       const titles = [...books].map((x) => x.querySelector("h2")!.innerHTML);
-      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design"]);
+      expect(titles).to.deep.equal([
+        "Refactoring",
+        "Domain-driven design",
+        "Building Microservices",
+      ]);
     });
   });
 });
